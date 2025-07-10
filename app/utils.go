0199a47d9d692975bb53595fa8db64447ec4f02a
Myoalp <corecybernetics354@gmail.com>
2025-07-10 19:03:57 +0700

    chore(utils): remove unused formatResponse

--- app/utils.go
+++ app/utils.go
@@ -1,27 +1,8 @@
 package app
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
 	"os"
 )
-
-func formatResponse(req *http.Request) string {
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Sprintf("Request error : %s", err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	respDump, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		return fmt.Sprintf("Response dump error : %s", err.Error())
-	}
-
-	return string(respDump)
-}
 
 func getDefaultEditor() string {
 	editor := os.Getenv("EDITOR")
