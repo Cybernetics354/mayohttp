1a9fd8781b07044ca591b4e3c1e0e0434256ae88
Myoalp <corecybernetics354@gmail.com>
2025-08-10 05:57:33 +0700

    feat(urlcompose): add helper indicator

--- app/urlcompose/view.go
+++ app/urlcompose/view.go
@@ -1,6 +1,9 @@
 package urlcompose
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/Cybernetics354/mayohttp/app/ui"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -19,7 +22,30 @@ func (m Model) View() string {
 
 func (m *Model) RenderUrl() string {
 	header := lipgloss.NewStyle().Padding(0, 1).Render("URL Result")
-	return ui.RenderWithHeader(container.Width(m.width).Render(m.url), header)
+	helperStyle := lipgloss.NewStyle().
+		Background(ui.FocusColor).
+		Align(lipgloss.Center)
+
+	var helper []string
+	if m.protocol != "" || len(m.paths) > 0 {
+		if m.protocol != "" {
+			comp := helperStyle.Width(len(m.protocol)).MarginRight(2).Render("0")
+			helper = append(helper, comp)
+		}
+
+		for i, path := range m.paths {
+			index := i + 1
+			comp := helperStyle.
+				Width(len(path)).
+				Render(fmt.Sprintf("%d", index))
+			helper = append(helper, comp)
+		}
+	}
+
+	view := container.Width(m.width).
+		Render(lipgloss.JoinVertical(lipgloss.Left, m.url, strings.Join(helper, " ")))
+
+	return ui.RenderWithHeader(view, header)
 }
 
 func (m *Model) RenderInput() string {
